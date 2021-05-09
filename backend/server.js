1d725ba3f930f1381dafb1a84b53ac10d397d2ff
Andrey <a.g.terentyev@gmail.com>
2021-05-09 09:49:12 +0300

    prepare deploy to heroku

--- backend/server.js
+++ backend/server.js
@@ -50,6 +50,21 @@ app.get("/api/get/:id", (req, res) => {
   }
 });
 
+// set static folders
+const __dirname = path.resolve();
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/build")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running...");
+  });
+}
+
 app.use(notFound);
 app.use(errorHandler);
 
